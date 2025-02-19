83edb781fdc2bd6a39ed0b56d394fcd72ae3892e
Jas <tinadumpschool@gmail.com>
2025-02-19 15:47:40 +0800

    Pinaka updated na files

--- src/app/reactive-validation/reactive-validation.component.ts
+++ src/app/reactive-validation/reactive-validation.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, Form } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-reactive-validation',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-validation.component.html',
   styleUrl: './reactive-validation.component.css'
 })
 export class ReactiveValidationComponent {
+loginForm: FormGroup;
 
+constructor(){
+  this.loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  })
+}
+
+onSubmit(){
+  if (this.loginForm.valid){
+    console.log("Form Submitted!", this.loginForm.value);
+  
+  } else{
+    console.log("Form is not valid");
+  }
+}
 }
